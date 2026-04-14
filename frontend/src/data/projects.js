aaf3e685bb248574c4db274b557e4f3a87d46d76
Harsh Pandey <hpandeyg16@gmail.com>
2026-04-15 01:42:38 +0530

    Update MERN Chat Application image URL

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -12,7 +12,7 @@ export const projectsData = [
     _id: "2",
     title: "MERN Chat Application",
     description: "A feature-rich, real-time messaging platform built using the MERN stack. It supports secure cross-domain authentication with JWT, real-time communication via Socket.io, and personalized user avatars through DiceBear. The application features a responsive UI built with Tailwind CSS, ensuring a seamless chat experience across all devices.",
-    image: "https://images.unsplash.com/photo-1611743572933-e7135821fbc1?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
+    image: "https://images.unsplash.com/photo-1662974770404-468fd9660389?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hhdCUyMGFwcGxpY2F0aW9ufGVufDB8fDB8fHww",
     techStack: ["MongoDB", "Express.js", "React.js", "Node.js", "Socket.io", "PeerJS", "Tailwind CSS", "JWT API"],
     githubLink: "https://github.com/harshiet16/Chatting-App",
     liveLink: "https://chatting-app-virid-two.vercel.app/login"
